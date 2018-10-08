6c62f4657eb1b5510feaf42ed9fe815b28b4a9c2
Marcus Brohede <a97marbr@gmail.com>
2018-10-08 14:42:16 +0200

    Boilerplate

---
+++ dbsk9_2018/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dbsk9_2018.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
